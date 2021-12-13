5f27b20180219eb738a58623685085e631eccfa7
Ostolski Marcin <ostolski.marcin@gmaail.com>
2021-12-13 14:20:06 +0100

    Initial Commit

---
+++ ZAD9/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ZAD9
+//
+//  Created by Marcin Ostolski on 13/12/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
